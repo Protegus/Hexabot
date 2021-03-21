b1caa0f860b15bb19eaf6d65f7ff3cb24f06fdf6
Protegus <kaidenarmstead06@gmail.com>
2021-03-20 17:30:34 -0700

    Created kick command

---
+++ src/commands/Moderation/kick.js
@@ -0,0 +1,50 @@
+const Eris = require('eris');
+
+const Command = require('../../base/Command');
+
+class Kick extends Command {
+    constructor (client) {
+        super(client, {
+            name: 'kick',
+            description: 'Kicks a member from a server.',
+            usage: 'kick <member> [reason]',
+            guildOnly: true,
+            args: true,
+            permLevel: 'Moderator',
+            aliases: ['boot']
+        });
+    }
+
+    async run (message, args) {
+        // args could be a mention, name, or user id
+        const member = this.client.resolveMember(message, args[0]);
+        if (!member) {
+            // return embedded message
+            const embed = new Eris.RichEmbed()
+                .setColor(this.client.enum.colors.RED)
+                .setTitle('Cannot Find Member!')
+                .setDescription('I cannot find this member! Try mentioning them or supplying their user ID!')
+                .setTimestamp();
+
+            return message.channel.createMessage({ embed: embed });
+        }
+
+        let description = `I have successfully kicked \`${member.username}#${member.discriminator}\``;
+
+        const reason = args[1] ? args.slice(1).join(' ') : null;
+        if (reason) description += `\n\nReason: \`${reason}\``;
+
+        member.kick(reason);
+
+        // return embedded message
+        const embed = new Eris.RichEmbed()
+            .setColor(this.client.enum.colors.GREEN)
+            .setTitle('Kicked Member!')
+            .setDescription(description)
+            .setTimestamp();
+        
+        message.channel.createMessage({ embed: embed });
+    }
+}
+
+module.exports = Kick;
